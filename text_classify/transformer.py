5eb503fce675fa8fc75fd1438c9443bfe83c6663
linkseed18612254945 <516947868@qq.com>
2020-04-24 18:33:41 +0800

    开始transformer

--- text_classify/transformer.py
+++ text_classify/transformer.py
@@ -1,9 +1,16 @@
 import torch.nn as nn
 import utils
 import torch
+from torch.nn import functional as F
+import math
 
-def attention(query, key, value):
-    x = torch.matmul()
+def attention(query, key, value, dropout=None):
+    weights = torch.matmul(query, torch.transpose(key, 2, 3)) / math.sqrt(query.shape[2])
+    weights = F.softmax(weights, dim=-1)
+    if dropout is not None:
+        weights = dropout(weights)
+    weighted_value = torch.matmul(weights, value)
+    return weighted_value, weights
 
 
 class Transformer(nn.Module):
@@ -25,19 +32,31 @@ class EncoderBox(nn.Module):
         self.feed_forward = feed_forward
 
 
-class SelfAttention(nn.Module):
-    def __init__(self, num_head, d_model):
-        super(SelfAttention, self).__init__()
+class MultiheadSelfAttention(nn.Module):
+    def __init__(self, num_head, d_model, dropout_rate=0.1):
+        super(MultiheadSelfAttention, self).__init__()
+        self.num_head = num_head
+        self.d_k = d_model / self.num_head
         self.Linears = utils.module_clone(nn.Linear(d_model, d_model), 3)
+        self.dropout = nn.Dropout(dropout_rate)
 
     def forward(self, query, key, value):
-        query = self.Linears[0](query)
-        key = self.Linears[1](key)
-        value = self.Linears[2](value)
-
-
+        query = self.Linears[0](query).reshape(query.shape[0], query.shape[1], self.num_head, self.d_k).transpose(1, 2)
+        key = self.Linears[1](key).reshape(key.shape[0], key.shape[1], self.num_head, self.d_k).transpose(1, 2)
+        value = self.Linears[2](value).reshape(value.shape[0], value.shape[1], self.num_head, self.d_k).transpose(1, 2)
+        weighted_value = attention(query, key, value, self.dropout)
+        weighted_value = torch.transpose(1, 2).reshape()
+        return weighted_value
 
 class FeedForward(nn.Module):
-    def __init__(self):
+    def __init__(self, d_model, d_ff, dropout_rate=0.1):
         super(FeedForward, self).__init__()
+        self.linear1 = nn.Linear(d_model, d_ff)
+        self.linear2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout_rate)
+
+    def forward(self, x):
+        output = self.linear2(self.linear1(x))
+        output = self.dropout(output)
+        return output
 
